3421e1b93114656acaad288b59505f9c0cca58d1
David Velasquez Rojas <drvelasquezr@outlook.com>
2023-10-17 15:57:29 -0500

    change script ps

--- Scripts/event_subs_create.ps1
+++ Scripts/event_subs_create.ps1
@@ -22,7 +22,7 @@ foreach ($row in $csvFile) {
     $includedEventTypes = $row.ID_includedEventTypes
 
     # Check if the Event Subscription already exists
-    $existingSubscription = Get-AzEventGridSubscription -ResourceGroupName $resourceGroupName -EventSubscriptionName $queueName -TopicName $topicName -ErrorAction SilentlyContinue
+    $existingSubscription = Get-AzEventGridSubscription -ResourceGroupName $resourcegroup -EventSubscriptionName $queueName -TopicName $topicName -ErrorAction SilentlyContinue
 
     if ($existingSubscription -ne $null) {
         Write-Host "Event Subscription '$queueName' already exists for topic '$topicName'. Skipping creation."
@@ -30,7 +30,7 @@ foreach ($row in $csvFile) {
     else {
 	
         # Check if the storage queue exists and if not, it will be created
-        $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+        $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourcegroup -Name $storageaccount
         $queue = Get-AzStorageQueue -Context $storageAccount.Context -Name $queueName 2>$null
 
         if ($queue -eq $null) {
@@ -38,19 +38,19 @@ foreach ($row in $csvFile) {
         }
 
         # Build the endpoint in the correct format
-        $endpoint = "/subscriptions/$subscriptionid/resourceGroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$storageAccountName/queueServices/default/queues/$queueName"
+        $endpoint = "/subscriptions/$subscriptionid/resourceGroups/$resourcegroup/providers/Microsoft.Storage/storageAccounts/$storageaccount/queueServices/default/queues/$queueName"
 
         # Create the subscription in Azure Event Grid with the TTL configured
         New-AzEventGridSubscription `
           -EventSubscriptionName $queueName `
-          -ResourceGroupName $resourceGroupName `
-          -TopicName $topicName `
+          -ResourceGroupName $resourcegroup `
+          -TopicName $topicname `
           -EndpointType StorageQueue `
           -Endpoint $endpoint `
           -IncludedEventType $includedEventTypes `
           -AdvancedFilteringOnArray `
           -StorageQueueMessageTtl $ttlInSeconds
 
-        Write-Host "Event Subscription '$queueName' created for topic '$topicName'."
+        Write-Host "Event Subscription '$queueName' created for topic '$topicname'."
     }
 }
